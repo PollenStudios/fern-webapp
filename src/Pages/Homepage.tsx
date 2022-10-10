ae31bebd30bfd162d3b31720a02de5f2ac5c7862
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-10 19:06:38 +0530

    created buttons

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Facebook from "../Assets/Icons/facebook.svg";
+import { ButtonWithIcon } from "../Components/Atoms/Buttons";
 
 const Homepage = () => {
-  return <div className="main-container heading-2">Hoasdfasdfsdasdfdsfmepage</div>;
+  return (
+    <div className="main-container heading-2">
+      <ButtonWithIcon variant="success" icon={Facebook} name="Facebook" />
+    </div>
+  );
 };
 
 export default Homepage;
