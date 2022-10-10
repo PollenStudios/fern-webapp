1c67ab547909379f3c4f32e04974f7736283b907
Mainka <mainka.dahnkher@hcode.tech>
2022-10-10 17:00:25 +0530

    navabr changes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar(): any {
                   <div className="flex items-center px-6 py-4 md:mx-auto md:max-w-3xl lg:mx-0 lg:max-w-none xl:px-0">
                     <div className="w-full">
                       <label htmlFor="search" className="sr-only font-inter">
-                        Search
+                        Explore
                       </label>
                       <div className="relative">
                         <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
@@ -49,7 +49,7 @@ export default function Navbar(): any {
                           id="search"
                           name="search"
                           className="block w-full rounded-full border border-gray-300 bg-white py-2 pl-10 pr-3 text-xs placeholder-gray-500 focus:border-indigo-500 focus:text-gray-900 focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-xs"
-                          placeholder="Search"
+                          placeholder="Explore"
                           type="search"
                         />
                       </div>
