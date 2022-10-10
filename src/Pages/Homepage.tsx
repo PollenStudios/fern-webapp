6b9aba22d2933e5e57e25c3381052533e80b3db5
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-10 19:08:21 +0530

    minor changes

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -3,11 +3,7 @@ import Facebook from "../Assets/Icons/facebook.svg";
 import { ButtonWithIcon } from "../Components/Atoms/Buttons";
 
 const Homepage = () => {
-  return (
-    <div className="main-container heading-2">
-      <ButtonWithIcon variant="success" icon={Facebook} name="Facebook" />
-    </div>
-  );
+  return <div className="main-container heading-2">Heading</div>;
 };
 
 export default Homepage;
