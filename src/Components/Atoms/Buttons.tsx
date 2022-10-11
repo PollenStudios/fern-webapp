6e5fb3c1dc28f4fcbee5655756a44487fe1042ef
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-11 11:07:26 +0530

    now button type is coming from props

--- src/Components/Atoms/Buttons.tsx
+++ src/Components/Atoms/Buttons.tsx
@@ -1,11 +1,11 @@
 import { addBgColor } from "../../Constants/Constants";
 
-export const Button = (props: { variant: string; name: string }) => {
-  const { variant, name } = props;
+export const Button = (props: { variant: string; name: string; type: any }) => {
+  const { variant, name, type } = props;
 
   return (
     <button
-      type="button"
+      type={type}
       className={`heading-6 inline-flex items-center px-6 py-3 border
        border-${variant === "outline" ? "primary" : "transparent"} text-base font-medium rounded-full shadow-sm ${
         variant === "outline" ? "text-primary" : `text-white ${addBgColor(variant)} focus:outline-none`
@@ -16,12 +16,12 @@ export const Button = (props: { variant: string; name: string }) => {
   );
 };
 
-export const ButtonWithIcon = (props: { variant: string; icon: string; name: string }) => {
-  const { variant, icon, name } = props;
+export const ButtonWithIcon = (props: { variant: string; icon: string; name: string; type: any }) => {
+  const { variant, icon, name, type } = props;
 
   return (
     <button
-      type="button"
+      type={type}
       className={`inline-flex items-center gap-x-4 pl-3 pr-6 py-3 border border-transparent text-base font-medium rounded-full shadow-sm text-white 
       ${addBgColor(variant)}  focus:outline-none`}
     >
