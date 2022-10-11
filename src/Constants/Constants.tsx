208776bbb643215a8c1effd78e490295540c3879
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-11 10:07:15 +0530

    fix:refactor the button class code with switch cases

--- src/Constants/Constants.tsx
+++ src/Constants/Constants.tsx
@@ -1,27 +1,42 @@
-import Twitter from '../Assets/Icons/twitter.svg';
-import Facebook from '../Assets/Icons/facebook.svg';
-import Instagram from '../Assets/Icons/instagram.svg';
-import Google from '../Assets/Icons/google.svg';
+import Twitter from "../Assets/Icons/twitter.svg";
+import Facebook from "../Assets/Icons/facebook.svg";
+import Instagram from "../Assets/Icons/instagram.svg";
+import Google from "../Assets/Icons/google.svg";
 
 export const socialMedia = [
   {
     title: Facebook,
-    name: 'Facebook',
-    href: '#',
+    name: "Facebook",
+    href: "#",
   },
   {
     title: Instagram,
-    name: 'Instagram',
-    href: '#',
+    name: "Instagram",
+    href: "#",
   },
   {
     title: Twitter,
-    name: 'Twitter',
-    href: 'https://twitter.com/PollenStudios',
+    name: "Twitter",
+    href: "https://twitter.com/PollenStudios",
   },
   {
     title: Google,
-    name: 'Google',
-    href: '#',
+    name: "Google",
+    href: "#",
   },
 ];
+
+export const addBgClass = (variant: string) => {
+  switch (variant) {
+    case "primary":
+      return "bg-primary";
+    case "danger":
+      return "bg-red-800";
+    case "success":
+      return "bg-secondary";
+    case "warning":
+      return "bg-yellow-500";
+    default:
+      console.log(`Variant name is incorrect ${variant}.`);
+  }
+};
