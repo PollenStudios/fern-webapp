83cb322e6ef5e5d00c44eef301818d696407c141
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-11 13:38:18 +0530

    new changes in input field

--- src/Components/Atoms/InputField.tsx
+++ src/Components/Atoms/InputField.tsx
@@ -1,18 +1,46 @@
 type InputProps = {
   label: string;
   type: string;
+  placeholder: string;
   className: string;
   register: any;
   required: boolean;
+  pattern: string;
 };
 
-const Input = ({ label, type, className, register, required }: InputProps) => {
+export const Input = ({ label, type, placeholder, className, register, required, pattern }: InputProps) => {
   return (
     <div className="flex flex-col gap-2">
       <label>{label}</label>
-      <input {...register(label, { required })} type={type} className={className} />
+      <input {...register(label, { required, pattern: { pattern } })} type={type} placeholder={placeholder} className={className} />
     </div>
   );
 };
 
-export default Input;
+export const TextArea = ({ label, type, placeholder, className, register, required, pattern }: InputProps) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label>{label}</label>
+      <textarea
+        {...register(label, { required, pattern: { pattern } })}
+        type={type}
+        placeholder={placeholder}
+        className={className}
+        rows="3"
+      />
+    </div>
+  );
+};
+
+export const Select = (label: any, className: string, register: any, required: boolean) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <label>{label}</label>
+      <select {...register(label, { required })} className={className}>
+        <option value="">Select...</option>
+        <option value="A">Category A</option>
+        <option value="B">Category B</option>
+      </select>
+    </div>
+  );
+};
--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import Input from "../Components/Atoms/InputField";
+import { Input, Select, TextArea } from "../Components/Atoms/InputField";
 
 const Homepage = () => {
   const { register, handleSubmit } = useForm();
@@ -9,15 +9,43 @@ const Homepage = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 w-60">
-      <Input
-        type="text"
-        label="First Name"
-        register={register}
-        required
-        className="w-48 p-2 paragraph-1  border-primary focus: outline-none  border "
-      />
-    </form>
+    <div className="main-container my-10">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+        <Input
+          type="text"
+          label="First Name"
+          placeholder="Enter your name"
+          register={register}
+          required
+          pattern=""
+          className="p-2 w-72 bg-gray-30 paragraph-1 rounded-sm border-primary  border focus:outline-none"
+        />
+        <Input
+          type="email"
+          label="Email"
+          placeholder="Enter your email "
+          register={register}
+          required
+          pattern=""
+          className="p-2 w-72 bg-gray-30 paragraph-1   rounded-sm border-primary  border focus:outline-none"
+        />
+        <TextArea
+          type="text"
+          label="Bio"
+          placeholder="Explain about yourself"
+          register={register}
+          required
+          pattern={""}
+          className="p-2 bg-gray-30 paragraph-1 rounded-sm border-primary  border focus:outline-none"
+        />
+        {/* <Select
+          label="select"
+          className="p-2 bg-gray-30 paragraph-1 rounded-sm border-primary  border focus:outline-none"
+          register={register}
+          required
+        /> */}
+      </form>
+    </div>
   );
 };
 
