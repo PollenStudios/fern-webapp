9d45466c6121b3f91dc96539c71fffcdfa20c3b6
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-17 18:35:13 +0530

    fix: navbar fixes

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -46,7 +46,7 @@ export default function Navbar(): any {
                         <input
                           id="search"
                           name="search"
-                          className="block w-3/4 md:w-1/2 paragraph-3 rounded-full border border-gray-300 bg-white py-2 pl-10 pr-3 text-xs placeholder-gray-500 focus:border-primary focus:text-primary focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-primary sm:text-xs"
+                          className="block w-3/4 md:w-1/2 paragraph-3 rounded-full border border-gray-300 bg-white py-2 pl-10 pr-3 text-xs placeholder-gray-500 focus:border-primary focus:text-primary focus:placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-primary sm:text-xs "
                           placeholder="Explore"
                           type="search"
                           onChange={(e) => console.log(e.target.value)}
