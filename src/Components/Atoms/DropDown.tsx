138abe8e617a6fc4d582639bd605db0b4f288b2f
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-18 19:33:49 +0530

    made a function of to return classes of dropdown selected item

--- src/Components/Atoms/DropDown.tsx
+++ src/Components/Atoms/DropDown.tsx
@@ -2,20 +2,20 @@ import { Key } from "react";
 
 //Dropdown shown with list item
 const DropDown = ({ list, addItem, selectedItems }: any) => {
+  const handleSelectedItem = (item: string) => {
+    if (selectedItems.includes(item)) {
+      return "border-l-2 bg-gray-20 border-primary cursor-not-allowed";
+    } else {
+      return "border-gray-100 cursor-pointer  rounded-t border-b hover:bg-primary hover:text-white";
+    }
+  };
+
   return (
-    <div id="dropdown" className="absolute shadow top-100 bg-white z-40 w-full   rounded max-h-select overflow-y-auto ">
+    <div id="dropdown" className="absolute shadow top-100 bg-white z-10 w-full   rounded max-h-select overflow-y-auto ">
       <div className="flex flex-col w-full">
         {list.map((item: string, key: Key) => {
           return (
-            <div
-              key={key}
-              className={`w-full ${
-                selectedItems.includes(item)
-                  ? `border-l-2 bg-gray-20 border-primary cursor-not-allowed`
-                  : `border-gray-100 cursor-pointer  rounded-t border-b hover:bg-primary hover:text-white`
-              }`}
-              onClick={() => !selectedItems.includes(item) && addItem(item)}
-            >
+            <div key={key} className={`w-full ${handleSelectedItem(item)}`} onClick={() => !selectedItems.includes(item) && addItem(item)}>
               <div className="w-full p-1 flex items-center">
                 <div className="mx-2  paragraph-2">{item}</div>
               </div>
