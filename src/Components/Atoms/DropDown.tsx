927d6271f1b6595b0ac8623ba85cfa11bf6698e2
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-10-21 12:53:59 +0530

    add types to dropdown elements

--- src/Components/Atoms/DropDown.tsx
+++ src/Components/Atoms/DropDown.tsx
@@ -1,10 +1,15 @@
 import { Key } from "react";
 
 //Dropdown shown with list item
-// ( list: any, addItem: (item: string) => void, selectedItems: string[] )
-const DropDown = ({ list, addItem, selectedItems }: any) => {
+type DropdownProps = {
+  list: string[] | undefined;
+  addItem: (item: string) => void;
+  selectedItems: string[] | undefined;
+};
+
+const DropDown = ({ list, addItem, selectedItems }: DropdownProps) => {
   const handleSelectedItem = (item: string) => {
-    if (selectedItems.includes(item)) {
+    if (selectedItems?.includes(item)) {
       return "border-l-2 bg-gray-20 border-primary cursor-not-allowed";
     } else {
       return "border-gray-100 cursor-pointer  rounded-t border-b hover:bg-primary hover:text-white";
@@ -14,9 +19,9 @@ const DropDown = ({ list, addItem, selectedItems }: any) => {
   return (
     <div id="dropdown" className="absolute shadow top-100 bg-white z-10 w-full   rounded max-h-select overflow-y-auto ">
       <div className="flex flex-col w-full">
-        {list.map((item: string, key: Key) => {
+        {list?.map((item: string, id: Key) => {
           return (
-            <div key={key} className={`w-full ${handleSelectedItem(item)}`} onClick={() => !selectedItems.includes(item) && addItem(item)}>
+            <div key={id} className={`w-full ${handleSelectedItem(item)}`} onClick={() => !selectedItems?.includes(item) && addItem(item)}>
               <div className="w-full p-1 flex items-center">
                 <div className="mx-2  paragraph-2">{item}</div>
               </div>
