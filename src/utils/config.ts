7570bdc99a857055d8b6b53fe897822fdddaef79
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-11-17 17:08:16 +0530

    changes in config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,15 +5,22 @@ export const POLYGON_MAINNET = {
   name: 'Polygon Mainnet',
   rpcUrls: { default: 'https://polygon-rpc.com' },
 };
-export const isMainNet = false;
 export const POLYGON_MUMBAI = {
   ...chain.polygonMumbai,
   name: 'Polygon Mumbai',
   rpcUrls: { default: 'https://rpc-mumbai.maticvigil.com' },
 };
+
+// const networkData = getNetworkDetails(process.env.netow)
+
+// getNetworkDetails = () =>{
+
+//   return
+// }
+
 const config = {
   lensUri: process.env.REACT_APP_LENS_API_URI ?? 'https://api.lens.dev',
-  isMainNet,
+  isMainNet: process.env.REACT_APP_IS_MAINNET,
   lensMumbaiUri: process.env.REACT_APP_LENS_API_MUMBAI_URI ?? 'https://api-mumbai.lens.dev',
   pinataJwt: process.env.REACT_APP_PINATA_JWT ?? '',
   w3StorageToken: process.env.REACT_APP_W3_STORAGE_TOKEN ?? '',
@@ -24,8 +31,9 @@ const config = {
   alchemyMainRpc: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
   alchemyMumbaiRpc: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.REACT_APP_ALCHEMY_KEY}`,
   public_Key: process.env.REACT_APP_BACKEND_PUBLIC_KEY,
-  APP_NAME: 'F3RN',
-  CHAIN_ID: isMainNet ? POLYGON_MAINNET.id : POLYGON_MUMBAI.id,
+  APP_NAME: process.env.REACT_APP_APP_NAME,
+  APP_NAME_FOR_LENS_API: process.env.REACT_APP_APP_NAME_TO_GET_DATA_FROM_LENS,
+  CHAIN_ID: process.env.REACT_APP_IS_MAINNET ? POLYGON_MAINNET.id : POLYGON_MUMBAI.id,
 };
 export const PageRoutes = {
   HOMEPAGE: '/',
@@ -42,7 +50,6 @@ export const PageRoutes = {
   ENABLE_DISPATCHER: '/enable-dispatcher',
   SOMETHING_WENT_WRONG: '/something-went-wrong',
 };
-export const IPFS_GATEWAY = 'https://lens.infura-ipfs.io/ipfs/';
 
 export const DEFAULT_CHAIN_IDS = ['0x13881', 80001, '80001'];
 
