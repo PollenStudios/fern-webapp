791ac2affcbfe3f7f2ee8decab59f60712bf4cdc
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-11-18 10:24:25 +0530

    add / in front of end points

--- src/API/apiRoutes.ts
+++ src/API/apiRoutes.ts
@@ -1,8 +1,8 @@
 export const apiRoutes = {
-  userProfileMe: 'user-profile/me/',
-  userProfile: 'user-profile/',
-  uploadMedia: 'upload-media/',
-  generateNonce: 'generate-nonce/',
-  generateToken: 'generate-token/',
-  artCategories: 'art-categories/',
+  userProfileMe: '/user-profile/me/',
+  userProfile: '/user-profile/',
+  uploadMedia: '/upload-media/',
+  generateNonce: '/generate-nonce/',
+  generateToken: '/generate-token/',
+  artCategories: '/art-categories/',
 };
