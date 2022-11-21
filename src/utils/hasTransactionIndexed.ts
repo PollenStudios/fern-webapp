45c2135571c9d550ad73230e0ab758255f601049
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-11-21 19:05:30 +0530

    clear console and decrease the call time to 1000ms

--- src/utils/hasTransactionIndexed.ts
+++ src/utils/hasTransactionIndexed.ts
@@ -16,13 +16,8 @@ export const pollUntilIndexed = async (input: { txHash: string } | { txId: strin
   try {
     while (true) {
       const response = await hasTxBeenIndexed(input);
-      console.log('pool until indexed: result', response);
 
       if (response.__typename === 'TransactionIndexedResult') {
-        console.log('pool until indexed: indexed', response.indexed);
-        console.log('pool until metadataStatus: metadataStatus', response.metadataStatus);
-
-        console.log('response.metadataStatus', response.metadataStatus);
         if (response.metadataStatus) {
           if (response.metadataStatus.status === 'SUCCESS') {
             return response;
@@ -39,7 +34,7 @@ export const pollUntilIndexed = async (input: { txHash: string } | { txId: strin
 
         console.log('pool until indexed: sleep for 1500 milliseconds then try again');
         // sleep for a second before trying again
-        await new Promise(resolve => setTimeout(resolve, 1500));
+        await new Promise(resolve => setTimeout(resolve, 1000));
       } else {
         // it got reverted and failed!
         throw response;
