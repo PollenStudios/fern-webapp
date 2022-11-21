9d9fe88cc1e463782c5e0aaa3646ee98579b4dd5
Rohan <geetanshuhcode@geetanshuhcode-Lenovo-IdeaPad-S145-15IWL.(none)>
2022-11-21 18:19:09 +0530

    code refactored of wealletcontextprovider

--- src/store/WalletContextProvider.tsx
+++ src/store/WalletContextProvider.tsx
@@ -71,35 +71,37 @@ const WalletProvider = ({ children }: any) => {
   };
 
   const connectToBrowserWallets = async () => {
-    if (localStorage.getItem('accessToken') && hasProfileState.hasProfile === false)
+    //user is connected but not have a profile
+    if (localStorage.getItem('accessToken') && hasProfileState.hasProfile === false) {
       return navigate(PageRoutes.SIGN_UP);
-
+    }
     setIsLoading(true);
 
     if (window.ethereum === 'undefined') {
       toast.error('Please Install Metamask');
       return false;
-    } else {
-      try {
-        dispatchAccount({ type: 'loading' });
-        const accounts = await window.ethereum.request({
-          // fetch metamask account ID/address
-          method: 'eth_requestAccounts',
-        });
-        walletProvider.current = window.ethereum;
-        if (accounts.length !== 0) {
-          dispatchAccount({ type: 'success', payload: accounts[0] });
-          account = accounts[0];
-          fetchWalletBalance(accounts[0]);
-          validateChain(accounts[0]);
-        } else toast.error('No account found');
-        setIsLoading(false);
-      } catch (error: any) {
-        console.log('error', error, error.message);
+    }
+    try {
+      dispatchAccount({ type: 'loading' });
+      const accounts = await window.ethereum.request({
+        method: 'eth_requestAccounts',
+      });
 
-        dispatchAccount({ type: 'error', payload: error });
-        setIsLoading(false);
+      if (accounts.length === 0) {
+        toast.error('No account found');
+        return false;
       }
+
+      walletProvider.current = window.ethereum;
+      dispatchAccount({ type: 'success', payload: accounts[0] });
+      account = accounts[0];
+      fetchWalletBalance(accounts[0]);
+      validateChain(accounts[0]);
+    } catch (error: any) {
+      toast.error(error.message);
+      dispatchAccount({ type: 'error', payload: error });
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -112,63 +114,12 @@ const WalletProvider = ({ children }: any) => {
       const balanceInEth = ethers.utils.formatEther(balance);
       dispatchWalletBalance({ type: 'success', payload: balanceInEth });
     } catch (error) {
-      if (process.env.NODE_ENV === 'development') {
-        console.log('error occurred', error);
-      }
+      console.log('error occurred', error);
       dispatchWalletBalance({ type: 'error', payload: error });
     }
   };
 
-  const validateChain = async (account: string) => {
-    const fetchChainId = walletProvider.current.chainId;
-    if (!DEFAULT_CHAIN_IDS.includes(fetchChainId)) {
-      if (switchNetwork) {
-        switchNetwork(config.chainId);
-        toast.error('Please change your network wallet!');
-      }
-    } else {
-      handleSign(account);
-    }
-  };
-
-  // Is wallet is connected
-
-  const handleAutoConnectWallet = () => {
-    if (window.ethereum) {
-      // window.ethereum
-      //   .request({ method: 'eth_accounts' })
-      //   .then(handleAccountsChanged)
-      //   .catch((err: any) => {
-      //     console.log(err, 'Not Connected');
-      //   });
-
-      window.ethereum.on('accountsChanged', handleAccountsChanged);
-      return true;
-    } else {
-      return false;
-    }
-  };
-
-  async function handleAccountsChanged() {
-    const accounts = await window.ethereum.request({
-      // fetch metamask account ID/address
-      method: 'eth_requestAccounts',
-    });
-    if (accounts.length === 0) {
-      dispatchAccount({ type: 'success', payload: '' });
-      dispatchIsLoggedIn({ type: 'success', payload: false });
-      clearStorage();
-      connectToBrowserWallets();
-    } else if (accounts[0] !== accountState.account) {
-      dispatchAccount({ type: 'success', payload: accounts[0] });
-      dispatchIsLoggedIn({ type: 'success', payload: false });
-      clearStorage();
-      connectToBrowserWallets();
-    }
-  }
-
   //signMessage
-
   const handleSign = async (address: string) => {
     try {
       // Get challenge
@@ -182,19 +133,25 @@ const WalletProvider = ({ children }: any) => {
       if (!challenge?.data?.challenge?.text) {
         return toast.error('Signature message is not valid');
       }
-      // Get signature
-      const signature = await signMessage(challenge?.data?.challenge?.text);
+      try {
+        // Get signature
+        const signature = await signMessage(challenge?.data?.challenge?.text);
 
-      // Auth user and set cookies
-      const auth = await authenticate({
-        variables: { request: { address, signature } },
-      });
-      toast.success('Connected');
+        // Auth user and set cookies
+        const auth = await authenticate({
+          variables: { request: { address, signature } },
+        });
+        toast.success('Connected');
+        localStorage.setItem('accessToken', auth.data?.authenticate.accessToken);
+        localStorage.setItem('refreshToken', auth.data?.authenticate.refreshToken);
+      } catch (error) {
+        dispatchHasProfile({ type: 'error', payload: error });
+        dispatchCurrentProfile({ type: 'error', payload: error });
+        dispatchUserSigNonce({ type: 'error', payload: error });
+        dispatchIsLoggedIn({ type: 'error', payload: error });
+        return;
+      }
 
-      localStorage.setItem('accessToken', auth.data?.authenticate.accessToken);
-      localStorage.setItem('refreshToken', auth.data?.authenticate.refreshToken);
-
-      // dispatchIsLoggedIn({ type: 'success', payload: true });
       const { data: profilesData } = await getProfiles({
         variables: { ownedBy: address },
       });
@@ -218,15 +175,17 @@ const WalletProvider = ({ children }: any) => {
         navigate(PageRoutes.SIGN_UP);
       } else {
         const profiles: any = profilesData?.profiles?.items;
-
         const generateNonceResult = await generateNonce(profiles[0].handle, account, profiles[0].id);
+
         //if backend does not have this profile but lens have then we will create it on backend
         //if generateNonce api response i user does not exist error,then it will return us true boolean
+
         if (generateNonceResult === true) {
           const formBodyData = new FormData();
           formBodyData.append('username', profiles[0].handle);
           formBodyData.append('wallet_address', account);
           formBodyData.append('lens_profile', profiles[0].id);
+
           const createUserResult = await createUser(formBodyData);
           //now if user is created on backend then generate it nonce and token and make him/her login
           if (createUserResult) {
@@ -237,7 +196,6 @@ const WalletProvider = ({ children }: any) => {
         generateNonceResult?.token && verifyBackendGeneratedToken(generateNonceResult?.token, profilesData);
       }
     } catch (error) {
-      // dispatchSignature({ type: 'error', payload:  error } );
       dispatchHasProfile({ type: 'error', payload: error });
       dispatchCurrentProfile({ type: 'error', payload: error });
       dispatchUserSigNonce({ type: 'error', payload: error });
@@ -246,11 +204,43 @@ const WalletProvider = ({ children }: any) => {
     }
   };
 
+  const validateChain = async (account: string) => {
+    const fetchChainId = walletProvider.current.chainId;
+    if (!DEFAULT_CHAIN_IDS.includes(fetchChainId)) {
+      if (switchNetwork) {
+        switchNetwork(config.chainId);
+        toast.error('Please change your network wallet!');
+      }
+    } else {
+      handleSign(account);
+    }
+  };
+
+  const verifyBackendGeneratedToken = async (token: string, profilesData: any) => {
+    if (token) {
+      const token = localStorage.getItem('backendToken');
+      const getProfileResult = await getBackendProfile(token);
+      dispatchCurrentProfile({
+        type: 'success',
+        payload: {
+          ...profilesData?.profiles?.items[0],
+          artistApprovalStatus: getProfileResult?.artist_approval_status,
+        },
+      });
+      dispatchIsLoggedIn({ type: 'success', payload: true });
+      dispatchHasProfile({ type: 'success', payload: true });
+      dispatchUserSigNonce({
+        type: 'success',
+        payload: { userSignNonce: profilesData?.userSigNonces?.lensHubOnChainSigNonce },
+      });
+    }
+  };
+
   const logout = () => {
     dispatchIsLoggedIn({ type: 'success', payload: false });
     dispatchCurrentProfile({ type: 'success', payload: {} });
     clearStorage();
-    navigate('/');
+    navigate(PageRoutes.DISCOVERY);
   };
 
   useEffect(() => {
@@ -258,11 +248,13 @@ const WalletProvider = ({ children }: any) => {
       logout();
     });
   }, [walletProvider.current.selectedAddress]);
+
   useEffect(() => {
     walletProvider.current.on('chainChanged', () => {
       logout();
     });
   }, [walletProvider.current.chainId]);
+
   return (
     <>
       <WalletContext.Provider
